d41b1a3b3a1f2894c2c32501de83441f47e2f21e
artistic-me <47886202+artistic-me@users.noreply.github.com>
2021-01-19 08:50:42 +0530

    Update app.module.ts

--- hello-prisma/src/app.module.ts
+++ hello-prisma/src/app.module.ts
@@ -3,13 +3,16 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {AppointmentModule} from './appointment/appointment.module';
+import {UserModule} from './user/user.module';
 
 @Module({
   imports: [
     AppointmentModule,
+    UserModule,
     GraphQLModule.forRoot({
       include: [
         AppointmentModule,
+        UserModule,
       ],
       debug: true,
       playground: true,
