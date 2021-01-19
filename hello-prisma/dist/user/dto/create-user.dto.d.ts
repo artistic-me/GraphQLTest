0e97099bf58e40661c23dadbefca97c17c9f2e6a
artistic-me <47886202+artistic-me@users.noreply.github.com>
2021-01-19 08:42:23 +0530

    Create create-user.dto.d.ts

---
+++ hello-prisma/dist/user/dto/create-user.dto.d.ts
@@ -0,0 +1,7 @@
+export declare class UserType {
+    readonly id: number;
+    readonly name: string;
+    readonly email: string;
+    readonly healthID: string;
+    readonly insuranceID?: string;
+}
